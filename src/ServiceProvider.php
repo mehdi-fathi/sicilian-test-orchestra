f9e55d83c44204828e9e487c9464a87b0ca357af
Mehdi <mehdifathi.developer@gmail.com>
2024-03-19 15:18:39 +0330

    Update:update code

---
+++ src/ServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SicilianTestOrchestra;
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+/**
+ *
+ */
+class SicilianTestOrchestraServiceProvider extends ServiceProvider
+{
+
+
+    /**
+     * Perform post-registration booting of services.
+     */
+    public function boot(): void
+    {
+
+        $this->publishes([
+            __DIR__ . '../Tests/Tests/migrations/2024_02_28_163404_report_tests.php' => database_path('migrations'),
+        ], 'migrations');
+
+    }
+
+}
